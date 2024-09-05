c203f3403a263817c329e0f2bf300d139b26b7d8
ddeiro <ddeiro@bdsdatasolution.com.br>
2024-09-05 18:49:31 -0300

    build: Added class "Fornecedor".

---
+++ src/core.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
+Base = declarative_base()
+
+class Fornecedor(Base):
+    __tablename__ = 'fornecedores'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50), nullable=False)
+    telefone = Column(String(20))
+    email = Column(String(50))
+    endereco = Column(String(100))
